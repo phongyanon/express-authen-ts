405fac1e21bcae3799655a8bd18d8b49cd49bae8
phongyanon <phongyanon@gmail.com>
2023-11-09 17:33:53 +0700

    [API] finish crud profile

--- src/v1/profile/services/mysql.ts
+++ src/v1/profile/services/mysql.ts
@@ -16,6 +16,7 @@ export class Query {
 					else {
 						resolve(rows.map( row => {
 							row.user_id = row.user_id.toString();
+							row.date_of_birth = row.date_of_birth.valueOf();
 							return row;
 						}));
 					}
@@ -32,6 +33,7 @@ export class Query {
 						if (row.length === 0) resolve({error: true, message: 'Profile: item does not exist'});
 						else {
 							row[0].user_id = row[0].user_id.toString();
+							row[0].date_of_birth = row[0].date_of_birth.valueOf();
 							resolve(row[0]);
 						}
 					}
@@ -41,6 +43,7 @@ export class Query {
 
 	addProfile(ctx: IProfileInsert){
 		return new Promise( resolve => {
+			let dateOfBirth = new Date(ctx.date_of_birth);
 			this.con.execute<ResultSetHeader>('INSERT INTO Profile ( \
 				user_id, \
 				first_name_EN, \
@@ -61,7 +64,7 @@ export class Query {
 				ctx.first_name_TH,
 				ctx.last_name_TH,
 				ctx.gender,
-				ctx.date_of_birth,
+				dateOfBirth,
 				ctx.address_EN,
 				ctx.address_TH,
 				ctx.zip_code,
@@ -103,7 +106,12 @@ export class Query {
 
 					set_field += `${field} = ?,`;
 					let key = field as string;
-					update_data.push(ctx[key as keyof IProfileUpdate]);
+					if (key === 'date_of_birth'){
+						let dateOfBirth = new Date(ctx[key as keyof IProfileUpdate] as number); 
+						update_data.push(dateOfBirth);
+					} else {
+						update_data.push(ctx[key as keyof IProfileUpdate]);
+					}
 				}
 			});
 			
