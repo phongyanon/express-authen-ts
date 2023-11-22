137e6857a9d990eaeaf1a1af9edf39268708814c
phongyanon <phongyanon@gmail.com>
2023-11-22 12:08:03 +0700

    [API] integrate upload with user profile

--- src/v1/file/controllers.ts
+++ src/v1/file/controllers.ts
@@ -2,6 +2,8 @@ import dotenv from 'dotenv';
 import { Request as Req } from "express";
 import { Query as UserMysqlQuery } from '../user/services/mysql';
 import { Query as UserMongoQuery } from '../user/services/mongo';
+import { Query as ProfileMysqlQuery } from '../profile/services/mysql';
+import { Query as ProfileMongoQuery } from '../profile/services/mongo';
 import { IResponse, ISuccessResponse, IUploadFileResp } from "../utils/common.type";
 import { Get, Post, Put, Delete, Route, SuccessResponse, Example, Path, Body, Request } from "tsoa";
 import { GoogleDriveService } from '../utils/googleDriveService';
@@ -15,36 +17,52 @@ googleDrive.authorize();
 @Route("v1")
 export class Controller {
   userQuery: any
+  profileQuery: any
   constructor(){
 		let UserQuery;
+		let ProfileQuery;
 		switch(process.env.DB_TYPE){
 				case('mysql'):
 					UserQuery = new UserMysqlQuery();
+					ProfileQuery = new ProfileMysqlQuery();
 					break;
 				case('mongo'):
 					UserQuery = new UserMongoQuery();
+					ProfileQuery = new ProfileMongoQuery();
 					break;
 		}
 		
+		this.profileQuery = ProfileQuery;
 		this.userQuery = UserQuery;
 	}
 
-	@Post("/upload/profile/image")
+	@Post("/upload/profile/image/{user_id}")
 	@SuccessResponse("200", "Upload image profile")
 	@Example<ISuccessResponse>({
 		message: 'success'
   })
-	uploadToGoogleDrive(@Request() req: Req ): Promise<IResponse | ISuccessResponse>{
+	uploadToGoogleDrive(@Request() req: Req, @Path() user_id: string ): Promise<IResponse | ISuccessResponse>{
 		return new Promise( async resolve => {
 			try {
 				const { file }: any = req;
-				console.log('file: ', file);
 				let now = new Date()
 				let file_name: string = `${now.valueOf().toString()}_${file.originalname}`;
 				let upload_result :any = await googleDrive.uploadFile(file_name, file.path);
 
 				if (upload_result.success === true){
-					resolve({message: upload_result.data});
+					let user_profile = await this.profileQuery.getProfileByUserId(user_id);
+					if (user_profile.hasOwnProperty('error')) resolve(user_profile);
+					else {
+
+						let updated_profile = await this.profileQuery.updateProfile({
+							id: user_profile.id.toString(),
+							image_profile: `https://drive.google.com/uc?export=view&id=${upload_result.data}`
+						});
+
+						if (updated_profile.hasOwnProperty('error')) resolve(updated_profile);
+						else resolve({message: upload_result.data});
+
+					}
 				} else {
 					resolve({error: true, message: upload_result.message});
 				}
--- src/v1/file/routes.ts
+++ src/v1/file/routes.ts
@@ -1,21 +1,27 @@
 import { Router, Request, Response } from "express";
-// import { Controller } from "./controllers";
-// import { IProfileInsert, IProfileUpdate, IProfile, IProfileUpdateByUser } from "./profile.type";
 import { uploadImageProfile } from "../middleware/fileFilter";
 import { Controller } from "./controllers";
 import { IResponse, ISuccessResponse } from "../utils/common.type";
 import { auth, checkRole, checkRoleUserAccess } from "../middleware/authen";
+import { Role } from "../utils/role";
 
 const router = Router();
 
 let fileController = new Controller();
 
-router.post("/upload/profile/image", uploadImageProfile, async (req: Request, res: Response) => {
+router.post("/upload/profile/image/:user_id", 
+		auth, 
+		checkRole([Role.SuperAdmin, Role.Admin, Role.User]), 
+		checkRoleUserAccess, 
+		uploadImageProfile,
+		async (req: Request, res: Response) => {
+
+	const user_id: string = (req.params.user_id).toString();
 	if (!req.file || Object.keys(req.file).length === 0) {
 		res.status(500).send({error: true, message: 'No file upload'});
 	}
 	else {
-		let result = await fileController.uploadToGoogleDrive(req);
+		let result: IResponse|ISuccessResponse = await fileController.uploadToGoogleDrive(req, user_id);
 		if (result.hasOwnProperty('error')) res.status(500).send(result);
 		else res.status(200).send(result);
 	}
